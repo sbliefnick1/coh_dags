e0b4481c82d67c13f4e2cd243e7f621c038674a4
Jeremy Harris <jharris@coh.org>
2021-11-12 10:03:56 -0800

    remove research security sync

--- hr_tableau_security_sync.py
+++ hr_tableau_security_sync.py
@@ -18,7 +18,7 @@ default_args = {
 dag = DAG('hr_tableau_security_sync', default_args=default_args, catchup=False, schedule_interval='0 21 * * *')
 
 hr_bash = 'cd C:\\Anaconda\\ETL\\tableau && python hr_security.py'
-epic_bash = 'cd C:\\Anaconda\\ETL\\tableau && python TableauEpicSecuritySync.py'
+# epic_bash = 'cd C:\\Anaconda\\ETL\\tableau && python TableauEpicSecuritySync.py'
 unlicense_bash = 'cd C:\\Anaconda\\ETL\\tableau && python Unlicense_Users.py'
 mf_sched = 'cd C:\\Anaconda\\ETL\\tableau && python MF_Schedulers_Security.py'
 
@@ -27,10 +27,10 @@ t1 = SSHOperator(ssh_conn_id='tableau_server',
                  command=hr_bash,
                  dag=dag)
 
-t2 = SSHOperator(ssh_conn_id='tableau_server',
-                 task_id='Sync_Epic_Users_And_Groups',
-                 command=epic_bash,
-                 dag=dag)
+# t2 = SSHOperator(ssh_conn_id='tableau_server',
+#                  task_id='Sync_Epic_Users_And_Groups',
+#                  command=epic_bash,
+#                  dag=dag)
 
 t3 = SSHOperator(ssh_conn_id='tableau_server',
                  task_id='Unlicense_Tableau_Users',
