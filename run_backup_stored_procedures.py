02b34920cf8f80219fc6487dd2e5d76f4b85cbec
Jeremy Harris <jercharris89@gmail.com>
2022-08-01 10:04:13 -0700

    Update run_backup_stored_procedures.py

--- run_backup_stored_procedures.py
+++ run_backup_stored_procedures.py
@@ -18,7 +18,9 @@ default_args = {
 dag = DAG('run_backup_stored_procedures', default_args=default_args, catchup=False, schedule_interval='0 21 * * *')
 
 dictionary_bash = 'cd C:\\Anaconda\\ETL\\misc_etl && conda activate foundation && python ebi_dictionary.py'
-version_bash = 'cd C:\\Anaconda\\ETL\\fi_dm_ebi && conda activate foundation && python version_stored_procedures.py && cd ebi-stored-procedures && git commit -am "add changes" && git push'
+changes_version_bash = 'cd C:\\Anaconda\\ETL\\fi_dm_ebi && conda activate foundation && python version_stored_procedures.py'
+commit_version_bash = 'cd C:\\Anaconda\\ETL\\fi_dm_ebi\\ebi-stored-procedures && git add -A && git commit -am "add changes"'
+push_version_bash = 'cd C:\\Anaconda\\ETL\\fi_dm_ebi\\ebi-stored-procedures && git push'
 
 d = SSHOperator(
     ssh_conn_id='tableau_server',
@@ -27,9 +29,30 @@ d = SSHOperator(
     dag=dag
 )
 
-v = SSHOperator(
+cv = SSHOperator(
     ssh_conn_id='tableau_server',
     task_id='run_versioning',
-    command=version_bash,
+    command=changes_version_bash,
+    dag=dag
+)
+
+cv = SSHOperator(
+    ssh_conn_id='tableau_server',
+    task_id='add_changes_to_versioning_folder',
+    command=changes_version_bash,
+    dag=dag
+)
+
+cmv = SSHOperator(
+    ssh_conn_id='tableau_server',
+    task_id='commit_versioning_changes_to_git',
+    command=commit_version_bash,
+    dag=dag
+)
+
+pv = SSHOperator(
+    ssh_conn_id='tableau_server',
+    task_id='push_versioning_changes_to_git',
+    command=push_version_bash,
     dag=dag
 )
