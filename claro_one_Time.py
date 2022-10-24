648ac21bc3edbc076871acce72d2c7191f521b71
Soren Bliefnick <sbliefnick@gmail.com>
2022-10-24 15:49:09 -0700

    Update for new batch

--- claro_one_Time.py
+++ claro_one_Time.py
@@ -17,37 +17,22 @@ default_args = {
 
 dag = DAG('claro_one_time', default_args=default_args, catchup=False, schedule_interval=None)
 
-sftp1 = SFTPOperator(task_id='202001-202004',
-                     ssh_conn_id='claro_sftp',
-                     local_filepath=f'/var/nfsshare/files/claro/Claro Healthcare Data Flat Month-202001-202004.xlsx',
-                     remote_filepath=f'/Claro Healthcare Data Flat Month-202001-202004.xlsx',
-                     create_intermediate_dirs=True,
-                     dag=dag)
+files = [
+    'Claro Healthcare Data Month -tbl-202001-202004.xlsx',
+    'Claro Healthcare Data Month -tbl-202005-202008.xlsx',
+    'Claro Healthcare Data Month -tbl-202009-202012.xlsx',
+    'Claro Healthcare Data Month -tbl-202101-202104.xlsx',
+    'Claro Healthcare Data Month -tbl-202105-202108.xlsx',
+    'Claro Healthcare Data Month -tbl-202109-202112.xlsx',
+    'Claro Healthcare Data Month -tbl-202201-202204.xlsx',
+    'Claro Healthcare Data Month -tbl-202205-202208.xlsx',
+    ]
 
-sftp2 = SFTPOperator(task_id='202005-202008',
-                     ssh_conn_id='claro_sftp',
-                     local_filepath=f'/var/nfsshare/files/claro/Claro Healthcare Data Flat Month-202005-202008.xlsx',
-                     remote_filepath=f'/Claro Healthcare Data Flat Month-202005-202008.xlsx',
-                     create_intermediate_dirs=True,
-                     dag=dag)
-
-sftp3 = SFTPOperator(task_id='202009-202012',
-                     ssh_conn_id='claro_sftp',
-                     local_filepath=f'/var/nfsshare/files/claro/Claro Healthcare Data Flat Month-202009-202012.xlsx',
-                     remote_filepath=f'/Claro Healthcare Data Flat Month-202009-202012.xlsx',
-                     create_intermediate_dirs=True,
-                     dag=dag)
-
-# sftp4 = SFTPOperator(task_id='202201-202204',
-#                      ssh_conn_id='claro_sftp',
-#                      local_filepath=f'/var/nfsshare/files/claro/Claro Healthcare Data Flat Month-202201-202204.xlsx',
-#                      remote_filepath=f'/Claro Healthcare Data Flat Month-202201-202204.xlsx',
-#                      create_intermediate_dirs=True,
-#                      dag=dag)
-#
-# sftp5 = SFTPOperator(task_id='202205-202208',
-#                      ssh_conn_id='claro_sftp',
-#                      local_filepath=f'/var/nfsshare/files/claro/Claro Healthcare Data Flat Month-202205-202208.xlsx',
-#                      remote_filepath=f'/Claro Healthcare Data Flat Month-202205-202208.xlsx',
-#                      create_intermediate_dirs=True,
-#                      dag=dag)
+for file in files:
+    sftp = SFTPOperator(task_id=file,
+                        ssh_conn_id='claro_sftp',
+                        local_filepath=f'/var/nfsshare/files/claro/{file}',
+                        remote_filepath=f'/{file}',
+                        create_intermediate_dirs=True,
+                        dag=dag)
+    sftp
