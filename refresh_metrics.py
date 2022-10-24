31dbd8fe99dfc38b6601029f1d01db600cd6d766
Soren Bliefnick <sbliefnick@gmail.com>
2022-10-24 15:49:21 -0700

    Bump priority weights

--- refresh_metrics.py
+++ refresh_metrics.py
@@ -33,80 +33,94 @@ refresh_access_operations_scorecard_table = 'cd C:\\Anaconda\\ETL\\metrics\\coll
 propagate_base_sql_views = 'cd C:\\Anaconda\\ETL\\metrics\\collections && conda activate metrics && python base_sql_propagation.py'
 refresh_parquet_files = 'cd C:\\Anaconda\\ETL\\metrics\\collections && conda activate metrics && python refresh_parquet_files.py'
 
+
 def refresh_ds(tableau_server, tableau_authentication, ds_luid):
     with server.auth.sign_in(tableau_authentication):
         server.datasources.refresh(ds_luid)
 
+
 gp = SSHOperator(ssh_conn_id='tableau_server',
-                task_id='git_pull_latest',
-                command=git_pull_latest,
-                dag=dag)
+                 task_id='git_pull_latest',
+                 command=git_pull_latest,
+                 dag=dag,
+                 priority_weight=100)
 
 rocdt = SSHOperator(ssh_conn_id='tableau_server',
-                task_id='refresh_oc_daily_financials_table',
-                command=refresh_oc_daily_financials_table,
-                dag=dag)
+                    task_id='refresh_oc_daily_financials_table',
+                    command=refresh_oc_daily_financials_table,
+                    dag=dag,
+                    priority_weight=100)
 
 rcfdft = SSHOperator(ssh_conn_id='tableau_server',
-                task_id='refresh_cfin_daily_flash_table',
-                command=refresh_cfin_daily_flash_table,
-                dag=dag)
+                     task_id='refresh_cfin_daily_flash_table',
+                     command=refresh_cfin_daily_flash_table,
+                     dag=dag,
+                     priority_weight=100)
 
 rmmt = SSHOperator(ssh_conn_id='tableau_server',
-                task_id='refresh_metrics_metadata_table',
-                command=refresh_metrics_metadata_table,
-                dag=dag)
+                   task_id='refresh_metrics_metadata_table',
+                   command=refresh_metrics_metadata_table,
+                   dag=dag,
+                   priority_weight=100)
 
 rqmst = SSHOperator(ssh_conn_id='tableau_server',
-                task_id='refresh_quality_monthly_scorecard_table',
-                command=refresh_quality_monthly_scorecard_table,
-                dag=dag)
+                    task_id='refresh_quality_monthly_scorecard_table',
+                    command=refresh_quality_monthly_scorecard_table,
+                    dag=dag,
+                    priority_weight=100)
 
 raost = SSHOperator(ssh_conn_id='tableau_server',
-                task_id='refresh_access_operations_scorecard_table',
-                command=refresh_access_operations_scorecard_table,
-                dag=dag)
+                    task_id='refresh_access_operations_scorecard_table',
+                    command=refresh_access_operations_scorecard_table,
+                    dag=dag,
+                    priority_weight=100)
 
 pbsv = SSHOperator(ssh_conn_id='tableau_server',
-                task_id='propagate_base_sql_views',
-                command=propagate_base_sql_views,
-                dag=dag)
+                   task_id='propagate_base_sql_views',
+                   command=propagate_base_sql_views,
+                   dag=dag,
+                   priority_weight=100)
 
 rpf = SSHOperator(ssh_conn_id='tableau_server',
-                task_id='refresh_parquet_files',
-                command=refresh_parquet_files,
-                dag=dag)
+                  task_id='refresh_parquet_files',
+                  command=refresh_parquet_files,
+                  dag=dag,
+                  priority_weight=100)
 
 rocde = PythonOperator(
         task_id='refresh_oc_daily_financials_extract',
         python_callable=refresh_ds,
-        op_kwargs={'tableau_server': server, 'tableau_authentication': auth, 'ds_luid': 'bfacbd49-df60-4dfa-aa4f-24006fb8952a'},
+        op_kwargs={'tableau_server': server, 'tableau_authentication': auth,
+                   'ds_luid': 'bfacbd49-df60-4dfa-aa4f-24006fb8952a'},
         dag=dag,
-        priority_weight=20,
+        priority_weight=100,
         )
 
 rcfdfe = PythonOperator(
         task_id='refresh_cfin_daily_flash_extract',
         python_callable=refresh_ds,
-        op_kwargs={'tableau_server': server, 'tableau_authentication': auth, 'ds_luid': 'fcfcba9e-023b-446f-929c-afc037c74b90'},
+        op_kwargs={'tableau_server': server, 'tableau_authentication': auth,
+                   'ds_luid': 'fcfcba9e-023b-446f-929c-afc037c74b90'},
         dag=dag,
-        priority_weight=20,
+        priority_weight=100,
         )
 
 rmme = PythonOperator(
         task_id='refresh_metrics_metadata_extract',
         python_callable=refresh_ds,
-        op_kwargs={'tableau_server': server, 'tableau_authentication': auth, 'ds_luid': 'e64852e7-d0cf-43ec-b3d1-55f6bdda421e'},
+        op_kwargs={'tableau_server': server, 'tableau_authentication': auth,
+                   'ds_luid': 'e64852e7-d0cf-43ec-b3d1-55f6bdda421e'},
         dag=dag,
-        priority_weight=20,
+        priority_weight=100,
         )
 
 raose = PythonOperator(
         task_id='refresh_access_operations_scorecard_extract',
         python_callable=refresh_ds,
-        op_kwargs={'tableau_server': server, 'tableau_authentication': auth, 'ds_luid': '97d0cf7d-eafb-4cca-a031-7b5c1d8ad799'},
+        op_kwargs={'tableau_server': server, 'tableau_authentication': auth,
+                   'ds_luid': '97d0cf7d-eafb-4cca-a031-7b5c1d8ad799'},
         dag=dag,
-        priority_weight=20
+        priority_weight=100
         )
 
 gp >> rpf
