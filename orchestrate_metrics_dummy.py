f4edaf388a37528007a183fe3c5443e53bb579c6
Jeremy Harris <jercharris89@gmail.com>
2023-01-20 23:38:30 -0800

    Create orchestrate_metrics_dummy.py

---
+++ orchestrate_metrics_dummy.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+import pendulum
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+import requests
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2018, 9, 12, tzinfo=pendulum.timezone('America/Los_Angeles')),
+    'concurrency': 8,
+    'email': ['jharris@coh.org'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+}
+
+with DAG('orchestrate_metrics_dummy', default_args=default_args, catchup=False, schedule_interval='0 1 * * *') as dag:
+
+    url = 'http://git.coh.org:7990/projects/EBI/repos/metrics/raw/artifacts/dag_helper.json?at=refs%2Fheads%2Fplatform_agnostic'
+    manifest = requests.get(url).json()
+    deps = requests.get(url).json()['base_to_collection_dependencies']
+    both = [d.split(" >> ") for d in deps]
+    bases = list(set([d[0].replace(".sql", "") for d in both]))
+    colls = list(set([d[1] for d in both]))
+
+    bs = {}
+    for base in bases:
+        task = DummyOperator(task_id = f'refresh_{base}_parquet')
+        bs[base] = task
+
+    cs = {}
+    for coll in colls:
+        task = DummyOperator(task_id = f'refresh_{coll}_collection')
+        cs[coll] = task
+
+    for b in both:
+        b[0] >> b[1]
