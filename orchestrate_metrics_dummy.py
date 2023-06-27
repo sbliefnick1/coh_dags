443b7f9adfe97596061a18ef41a53f534698aefa
Jeremy Harris <jercharris89@gmail.com>
2023-06-26 17:10:39 -0700

    use real metrics

--- orchestrate_metrics_dummy.py
+++ orchestrate_metrics_dummy.py
@@ -19,29 +19,18 @@ default_args = {
 
 with DAG('orchestrate_metrics_dummy', default_args=default_args, catchup=False, schedule_interval='0 1 * * *') as dag:
 
-    url = 'http://git.coh.org:7990/projects/EBI/repos/metrics/raw/artifacts/dag_helper.json?at=refs%2Fheads%2Fplatform_agnostic'
-    manifest = requests.get(url).json()
-    deps = requests.get(url).json()['base_to_collection_dependencies']
-    both = [d.split(" >> ") for d in deps]
+    url = 'https://vpxrstudio.coh.org/content/5fceaff8-8811-41ac-be8b-88aae904b2b6/nodes/'
+    data = requests.get(url).json()
 
-    def clean_name(n):
-        return n.replace(".sql", "").replace(" ", "_")
+    nodes = list(set([f"{n['type']}_{n['name']}" for n in data]))
 
-    bases = list(set([f'base-{clean_name(d[0])}' for d in both]))
-    colls = list(set([f'collection-{clean_name(d[1])}' for d in both]))
+    n = {}
+    for node in nodes:
+        task = DummyOperator(task_id=node)
+        n[node] = task
 
-    bs = {}
-    for base in bases:
-        task = DummyOperator(task_id=base)
-        bs[base] = task
-
-    cs = {}
-    for coll in colls:
-        task = DummyOperator(task_id=coll)
-        cs[coll] = task
-
-    print(bs)
-    for d in both:
-        f = f'base-{clean_name(d[0])}'
-        l = f'collection-{clean_name(d[1])}'
-        bs.get(f) >> cs.get(l)
+    for node in nodes:
+        for parent in node['parents']:
+            f = f"{node['type']}_{node['name']}"
+            l = f"{parent['type']}_{parent['name']}"
+            n.get(f) >> n.get(l)
