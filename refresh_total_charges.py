d024987139db8442b8504c21c3f5225a58423396
Jeremy Harris <jercharris89@gmail.com>
2023-09-29 10:00:38 -0700

    add total charges extracts back

--- refresh_total_charges.py
+++ refresh_total_charges.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 import pendulum
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.microsoft.mssql.operators.mssql import MsSqlOperator
 from auxiliary.outils import refresh_tableau_extract
 
 default_args = {
@@ -19,14 +18,36 @@ default_args = {
 
 with DAG('refresh_total_charges', default_args=default_args, catchup=False, schedule_interval='0 17 * * *') as dag:
 
-    conn_id = 'ebi_datamart'
-    pool_id = 'ebi_etl_pool'
-
-    rvus = PythonOperator(
+    rvu = PythonOperator(
         task_id='refresh_rvu_extract',
         python_callable=refresh_tableau_extract,
         op_kwargs={'datasource_id': 'c08148a1-cf27-48df-8c8f-fc29f2c77c12'},
-        dag=dag
     )
 
-    rvus
+    tc = PythonOperator(
+        task_id='refresh_total_charges',
+        python_callable=refresh_tableau_extract,
+        op_kwargs={'datasource_id': 'ad80c2d3-3926-4516-af11-55bac64735ac'},
+    )
+
+    new = PythonOperator(
+        task_id='refresh_new_em_provider',
+        python_callable=refresh_tableau_extract,
+        op_kwargs={'datasource_id': 'ea9a44b2-3f27-4560-9a00-4e056ede95bd'},
+    )
+
+    trj = PythonOperator(
+        task_id='refresh_patient_trajectory',
+        python_callable=refresh_tableau_extract,
+        op_kwargs={'datasource_id': '951a76e5-e7a7-466a-a0aa-6b592bd9a370'},
+    )
+
+    cim = PythonOperator(
+        task_id='refresh_cim_charges',
+        python_callable=refresh_tableau_extract,
+        op_kwargs={'datasource_id': '6e39fb3d-fdc1-41ee-8a2a-3f819a12fea5'},
+    )
+
+    rvu
+    tc >> new >> trj
+    tc >> cim
