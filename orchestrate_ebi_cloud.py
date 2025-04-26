42627f67b00890c983b1543aab0bd7434d622572
Jeremy Harris <jercharris89@gmail.com>
2025-04-25 19:56:31 -0700

    Update orchestrate_ebi_cloud.py

--- orchestrate_ebi_cloud.py
+++ orchestrate_ebi_cloud.py
@@ -24,6 +24,7 @@ with DAG('orchestrate_ebi_cloud', default_args=default_args, catchup=False, sche
     colls_enviro = 'dbt_cloud'
 
     wait_for_sources_bash = f'cd {repo} && conda activate {enviro} && python wait_for_sources.py'
+    wait_for_ebi_sources_bash = f'cd {repo} && conda activate {enviro} && python wait_for_ebi_sources.py'
     ae_dbt_build_bash = f'cd {repo} && conda activate {enviro} && python ae_dbt_build.py'
     ebi_dbt_build_bash = f'cd {repo} && conda activate {enviro} && python ebi_dbt_build.py'
     cfin_dbt_build_bash = f'cd {repo} && conda activate {enviro} && python cfin_dbt_build.py'
@@ -34,6 +35,12 @@ with DAG('orchestrate_ebi_cloud', default_args=default_args, catchup=False, sche
         ssh_conn_id='tableau_server',
         task_id='wait_for_sources',
         command=wait_for_sources_bash,
+    )
+
+    wait_for_ebi_sources = SSHOperator(
+        ssh_conn_id='tableau_server',
+        task_id='wait_for_ebi_sources',
+        command=wait_for_ebi_sources_bash,
     )
 
     ae_dbt_build = SSHOperator(
@@ -61,5 +68,7 @@ with DAG('orchestrate_ebi_cloud', default_args=default_args, catchup=False, sche
     )
 
     wait_for_sources >> ae_dbt_build
-    ae_dbt_build >> ebi_dbt_build >> refresh_tableau_extracts
+    ae_dbt_build >> ebi_dbt_build
+    wait_for_ebi_sources >> ebi_dbt_build
+    ebi_dbt_build >> refresh_tableau_extracts
     ae_dbt_build >> cfin_dbt_build
