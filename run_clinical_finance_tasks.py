4237a7dff48e16675c869787e6440762659edc63
Jeremy Harris <jercharris89@gmail.com>
2025-07-05 16:49:58 -0700

    Update run_clinical_finance_tasks.py

--- run_clinical_finance_tasks.py
+++ run_clinical_finance_tasks.py
@@ -31,18 +31,10 @@ with DAG('run_clinical_finance_tasks', default_args=default_args, catchup=False,
     cfin_repo = f'{repo}\\clinical_finance'
     enviro = 'ebi_data_engineering'
 
-    git_pull_bash = f'cd {repo} && git pull'
     refresh_maps_bash = f'cd {cfin_repo} && conda activate {enviro} && python mappings_to_ebi.py'
 
-    git = SSHOperator(
-        ssh_conn_id='tableau_server',
-        task_id='git_pull_latest',
-        command=git_pull_bash,
-        dag=dag,
-    )
-
     maps = SSHOperator(
-        ssh_conn_id='tableau_server',
+        ssh_conn_id='ebi_etl_server',
         task_id='refresh_mapping_tables',
         command=refresh_maps_bash,
         dag=dag,
@@ -71,8 +63,6 @@ with DAG('run_clinical_finance_tasks', default_args=default_args, catchup=False,
         dag=dag,
     )
 
-    git >> maps
-    git >> ebi
-
+    maps
     ebi >> tab
     ebi >> fter
