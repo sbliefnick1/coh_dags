7bcfe0b60527a64c003ee67028605803e9cfb266
Jeremy Harris <jercharris89@gmail.com>
2025-07-05 16:28:28 -0700

    Update refresh_core_extracts.py

--- refresh_core_extracts.py
+++ refresh_core_extracts.py
@@ -18,26 +18,26 @@ default_args = {
 with DAG('refresh_core_extracts', default_args=default_args, catchup=False, schedule_interval='0 16 * * *') as dag:
 
     repo = 'C:\\Users\\ebitabuser\\Documents\\ebi-automations'
-    enviro = 'ebi_automations'
+    enviro = 'dbt_automations'
 
     run_iip_extracts_bash = f'cd {repo} && conda activate {enviro} && python refresh_iip_time_to_seen_metrics.py'
     run_dbt_common_coverage_bash = f'cd {repo} && conda activate {enviro} && python get_coverage_stats.py'
     run_dbt_housekeeping_bash = f'cd {repo} && conda activate {enviro} && python ebi_dbt_housekeeping.py'
     
     run_iip_extracts = SSHOperator(
-        ssh_conn_id='tableau_server',
+        ssh_conn_id='ebi_etl_server',
         task_id='run_iip_extracts',
         command=run_iip_extracts_bash,
     )
 
     run_dbt_common_coverage_refresh = SSHOperator(
-        ssh_conn_id='tableau_server',
+        ssh_conn_id='ebi_etl_server',
         task_id='run_dbt_common_coverage_refresh',
         command=run_dbt_common_coverage_bash,
     )
 
     run_ebi_dbt_housekeeping = SSHOperator(
-        ssh_conn_id='tableau_server',
+        ssh_conn_id='ebi_etl_server',
         task_id='run_dbt_housekeeping_refresh',
         command=run_dbt_housekeeping_bash,
     )
