ad44f9fb2acabe00e7219180b57e7a3cca5a62cb
Jeremy Harris <jercharris89@gmail.com>
2025-07-07 16:54:37 -0700

    Update refresh_core_extracts.py

--- refresh_core_extracts.py
+++ refresh_core_extracts.py
@@ -15,38 +15,35 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
-with DAG('refresh_core_extracts', default_args=default_args, catchup=False, concurrency=1, schedule_interval='0 16 * * *') as dag:
+with DAG('refresh_core_extracts', default_args=default_args, catchup=False, concurrency=4, schedule_interval='0 16 * * *') as dag:
 
-    repo = 'C:\\Users\\ebitabuser\\Documents\\ebi-automations'
+    repo = r'C:\Users\ebitabuser\Documents\ebi-automations'
     enviro = 'dbt_automations'
+    python_exe = rf'C:\Users\ebitabuser\AppData\Local\Miniconda3\envs\{enviro}\python.exe'
+    prefix = f'cd {repo} && "{python_exe}"'
 
-    run_iip_extracts_bash = f'cd {repo} && conda run -n {enviro} python refresh_iip_time_to_seen_metrics.py'
-    run_dbt_common_coverage_bash = f'cd {repo} && conda run -n {enviro} python get_coverage_stats.py'
-    run_dbt_housekeeping_bash = f'cd {repo} && conda run -n {enviro} python ebi_dbt_housekeeping.py'
-    run_ebi_snowflake_progress_bash = f'cd {repo} && conda run -n {enviro} python ebi_snowflake_progress.py'
-    
     run_iip_extracts = SSHOperator(
         ssh_conn_id='ebi_etl_server',
         task_id='run_iip_extracts',
-        command=run_iip_extracts_bash,
+        command=f'{prefix} refresh_iip_time_to_seen_metrics.py',
     )
 
     run_dbt_common_coverage_refresh = SSHOperator(
         ssh_conn_id='ebi_etl_server',
         task_id='run_dbt_common_coverage_refresh',
-        command=run_dbt_common_coverage_bash,
+        command=f'{prefix} get_coverage_stats.py',
     )
 
     run_ebi_dbt_housekeeping = SSHOperator(
         ssh_conn_id='ebi_etl_server',
         task_id='run_dbt_housekeeping_refresh',
-        command=run_dbt_housekeeping_bash,
+        command=f'{prefix} ebi_dbt_housekeeping.py',
     )
 
     run_ebi_snowflake_progress = SSHOperator(
         ssh_conn_id='ebi_etl_server',
         task_id='run_ebi_snowflake_progress_refresh',
-        command=run_ebi_snowflake_progress_bash,
+        command=f'{prefix} ebi_snowflake_progress.py',
     )
     
     run_iip_extracts
