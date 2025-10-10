d9d219a4d1c9e868ffe0e73ae68398cf16f1e0c3
Jeremy Harris <jercharris89@gmail.com>
2025-10-10 12:17:34 -0700

    Update run_refresh_other_extracts.py
    remove archived data sources

--- run_refresh_other_extracts.py
+++ run_refresh_other_extracts.py
@@ -32,14 +32,8 @@ datasources = [
      'datasource_id': '46da3492-9204-401e-b46f-a31ba2aaf75d'},
     {'task_id': 'refresh_corporate_accounting_accounts_payable',
      'datasource_id': 'c30f1707-ad4a-449c-8150-111575700865'},
-    {'task_id': 'refresh_daily_its_calls_and_tickets',
-     'datasource_id': '9b0f200d-4eb6-4c8c-a581-0c0383b6d1ff'},
     {'task_id': 'refresh_capital_project_security_check',
      'datasource_id': '23d5115f-eadc-4366-95b7-2a82aa710056'},
-    {'task_id': 'refresh_revenue_cycle_pb_workqueue_telehealth',
-     'datasource_id': '3fd8f8f1-439c-499c-9649-e15a3251bd35'},
-    {'task_id': 'refresh_corporate_accounting_expense_reimbursement',
-     'datasource_id': 'bb771489-e5a1-45c0-943b-7c8a02ad7aac'},
     {'task_id': 'refresh_pb_tdl_transactions',
      'datasource_id': '8a83879d-6937-4a31-a784-21107733854f'},
     {'task_id': 'refresh_corporate_accounting_ap_aging',
@@ -50,10 +44,6 @@ datasources = [
      'datasource_id': 'a81be1ce-498c-4a79-8730-5fdcc20a1cec'},
     {'task_id': 'refresh_340B_dsh_patient_days',
      'datasource_id': '407119d8-7720-4304-9d97-776fcb65c987'},
-    {'task_id': 'refresh_dbt_run_results',
-     'datasource_id': '041df7f7-d700-45db-a6ca-f0a29d9a4260'},
-    {'task_id': 'refresh_dbt_test_results',
-     'datasource_id': '9d73ac49-14d2-4695-ac5e-b1b0640bfe81'},
     {'task_id': 'refresh_spec_pharmacy_credit_card',
      'datasource_id': '62f4c05c-355c-472a-a98f-df48dfc3fdbf'},
     {'task_id': 'refresh_ccsg_publications',
@@ -69,14 +59,3 @@ for d in datasources:
             )
 
 task
-
-claro = MsSqlOperator(
-        sql='exec EBI_Claro_Patient_Account_File_Extract_Logic',
-        task_id='exec_EBI_Claro_Patient_Account_File_Extract_Logic',
-        autocommit=True,
-        mssql_conn_id='ebi_datamart',
-        pool='ebi_etl_pool',
-        dag=dag
-        )
-
-claro
