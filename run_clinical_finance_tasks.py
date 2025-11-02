e1ca84e237276cd51b4eb935f96e8435ef10efac
Jeremy Harris <jercharris89@gmail.com>
2025-11-02 14:25:41 -0800

    Remove SSH and Python operators from task flow

--- run_clinical_finance_tasks.py
+++ run_clinical_finance_tasks.py
@@ -32,13 +32,6 @@ with DAG('run_clinical_finance_tasks', default_args=default_args, catchup=False,
     python_exe = rf'C:\Users\ebitabuser\AppData\Local\Miniconda3\envs\{enviro}\python.exe'
     prefix = f'cd {repo} && "{python_exe}"'
 
-    maps = SSHOperator(
-        ssh_conn_id='ebi_etl_server',
-        task_id='refresh_mapping_tables',
-        command=f'{prefix} mappings_to_ebi.py',
-        dag=dag,
-    )
-
     ebi = MsSqlOperator(
         sql=ebi_sql,
         task_id='refresh_labor_table_in_ebi',
@@ -47,21 +40,3 @@ with DAG('run_clinical_finance_tasks', default_args=default_args, catchup=False,
         pool=pool_id,
         dag=dag,
     )
-
-    tab = PythonOperator(
-        task_id='refresh_labor_table_in_tableau',
-        python_callable=refresh_tableau_extract,
-        op_kwargs={'datasource_id': '149fbbfa-b146-454e-be88-f7c365ccafbe'},
-        dag=dag,
-    )
-
-    fter = PythonOperator(
-        task_id='refresh_labor_fte',
-        python_callable=refresh_tableau_extract,
-        op_kwargs={'datasource_id': 'DE1BE98A-B246-4317-9223-97C2F533EB43'},
-        dag=dag,
-    )
-
-    maps
-    ebi >> tab
-    ebi >> fter
